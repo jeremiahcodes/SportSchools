7889efe0d4f25d466a12836b83f1918fffc395ed
jeremiahew <jeremiahewdev@gmail.com>
2023-10-11 17:53:28 -0400

    init

---
+++ src/main/java/com/jeremiahcodes/sportschools/SportschoolsApplication.java
@@ -0,0 +1,13 @@
+package com.jeremiahcodes.sportschools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportschoolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportschoolsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jeremiahcodes/sportschools/SportschoolsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeremiahcodes.sportschools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportschoolsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
